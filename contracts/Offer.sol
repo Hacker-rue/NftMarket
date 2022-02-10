3998b81bb8fef732d5d5a221af79c12013994630
Hacker-rue <pavlik.soldatov.12@gmail.com>
2022-02-10 14:57:26 +0400

    version 1.2.2 path 2

---
+++ contracts/Offer.sol
@@ -0,0 +1,33 @@
+pragma ton-solidity 0.49.0;
+
+pragma AbiHeader time;
+pragma AbiHeader expire;
+pragma AbiHeader pubkey;
+
+contract Offer {
+
+    address static _addrOwner;
+    address static _addrNft;
+
+    address _addrNftMarket;
+
+    uint128 _price;
+
+    constructor (uint128 price) public {
+        
+    }
+
+    function setNft() public onlyOwner() {
+
+    }
+
+
+
+
+    modifier onlyOwner() {
+        require(msg.sender == _addrOwner);
+        _;
+    }
+
+
+}
